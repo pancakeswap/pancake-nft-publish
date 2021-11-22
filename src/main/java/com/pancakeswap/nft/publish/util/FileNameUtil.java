6574262cb4e80a694ca950f00e6b584f82bbb002
Chef Huan <huan@pancakeswap.com>
2021-11-22 12:56:23 +0200

    fix(token name): add camelCase regex pattern

--- src/main/java/com/pancakeswap/nft/publish/util/FileNameUtil.java
+++ src/main/java/com/pancakeswap/nft/publish/util/FileNameUtil.java
@@ -5,7 +5,9 @@ import java.util.Locale;
 public class FileNameUtil {
 
     public static String formattedTokenName(String name) {
-        return name.replaceAll("[^a-zA-Z0-9]", " ")
+        return name
+                .replaceAll("([a-z])([A-Z]+)", "$1-$2")
+                .replaceAll("[^a-zA-Z0-9]", " ")
                 .trim().replaceAll(" +", " ")
                 .replaceAll(" ", "-")
                 .toLowerCase(Locale.ROOT);
