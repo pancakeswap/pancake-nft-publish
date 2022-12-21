ba35c7c3ba68b4d35577e3ddef46c21fb2bd4b3d
Chef Huan <huan@pancakeswap.com>
2022-12-21 17:40:17 +0100

    feat: basic auth

--- src/main/java/com/pancakeswap/nft/publish/controller/CollectionController.java
+++ src/main/java/com/pancakeswap/nft/publish/controller/CollectionController.java
@@ -8,6 +8,7 @@ import com.pancakeswap.nft.publish.service.NFTService;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,9 @@ import java.time.Duration;
 
 @RestController
 public class CollectionController {
+
+    @Value(value = "${secure.token}")
+    public String accessToken;
 
     private final NFTService nftService;
     private final DBService dbService;
@@ -33,31 +37,43 @@ public class CollectionController {
     }
 
     @PostMapping(path = "/collections")
-    public ResponseEntity<String> listCollection(@Valid @RequestBody CollectionDataDto dataDto) {
-        if (bucket.tryConsume(1)) {
-            try {
-                FutureConfig config = FutureConfig.init();
-                nftService.storeAvatarAndBanner(config, dataDto.getAddress(), dataDto.getAvatarUrl(), dataDto.getBannerUrl());
-                String result = nftService.listNFT(config, dataDto, 0);
-                return ResponseEntity.ok(result);
-            } catch (Exception ex) {
-                throw new ListingException("Failed to list collection");
+    public ResponseEntity<String> listCollection(@Valid @RequestBody CollectionDataDto dataDto,
+                                                 @RequestHeader(value = "x-secure-token") String token) {
+        if (isValidToken(token)) {
+            if (bucket.tryConsume(1)) {
+                try {
+                    FutureConfig config = FutureConfig.init();
+                    nftService.storeAvatarAndBanner(config, dataDto.getAddress(), dataDto.getAvatarUrl(), dataDto.getBannerUrl());
+                    String result = nftService.listNFT(config, dataDto, 0);
+                    return ResponseEntity.ok(result);
+                } catch (Exception ex) {
+                    throw new ListingException("Failed to list collection");
+                }
             }
-        }
 
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @DeleteMapping(path = "/collections/{id}")
-    public ResponseEntity<String> deleteCollection(@PathVariable("id") String id) {
-        if (bucket.tryConsume(1)) {
-            try {
-                dbService.deleteCollection(id);
-                return ResponseEntity.ok("Done");
-            } catch (Exception ex) {
-                throw new ListingException("Failed to list collection");
+    public ResponseEntity<String> deleteCollection(@PathVariable("id") String id,
+                                                   @RequestHeader(value = "x-secure-token") String token) {
+        if (isValidToken(token)) {
+            if (bucket.tryConsume(1)) {
+                try {
+//                    dbService.deleteCollection(id);
+                    return ResponseEntity.ok("Done");
+                } catch (Exception ex) {
+                    throw new ListingException("Failed to list collection");
+                }
             }
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    private boolean isValidToken(String token) {
+        return accessToken.equals(token);
     }
 }
